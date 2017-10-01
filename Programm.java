cbfd2e65e85ff46ad478392ce887332cba39efca
Gosha20 <gosha3548@gmail.com>
2017-10-01 18:18:48 +0500

    test change

--- Programm.java
+++ Programm.java
@@ -1,15 +1,22 @@
 package Snake;
 
+import java.util.concurrent.TimeUnit;
+
 public class Programm {
-    public static void main(String[] args) {
-        Game game = new Game(3, 5);
+    public static void clearScreen() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
+    public static void main(String[] args) throws InterruptedException {
+        Game game = new Game(5, 5);
         game.Course = Game.eCourse.DOWN;
         game.Print();
         System.out.println();
 
-        for(int i=0;i<3;i++){
+        for(int i=0;i<100;i++){
             game.RefreshField();
             game.Print();
-            System.out.println();}
+            System.out.println();
+        }
     }
 }
