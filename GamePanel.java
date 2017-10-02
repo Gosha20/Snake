6c9ca0a3e0768cdf806f81897905adda00a4c938
Gosha20 <gosha3548@gmail.com>
2017-10-02 10:37:29 +0500

    Change class

--- GamePanel.java
+++ GamePanel.java
@@ -1,17 +1,14 @@
 package Snake;
 
 import java.awt.*;
-import java.util.concurrent.TimeUnit;
-import java.awt.event.*;
 import javax.swing.*;
 
-public class GameApp extends JFrame {
-    Game game;
+public class GamePanel extends JFrame {
+    GameModel game;
     Graphics graphics;
-    public GameApp(){
-
-        super("Game Snake");
-        game = new Game(10,10);
+    public GamePanel(){
+        super("GameModel Snake");
+        game = new GameModel(10,10);
         JPanel graphicsPanel = new JPanel();
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setSize(200,200);
