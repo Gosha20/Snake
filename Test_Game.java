1f102d2c156047f9bcbe3de5dd1692958a14145e
Ilya Orlov <ilyaorlov1998@gmail.com>
2017-10-09 00:44:36 +0500

    Remade and new tests

--- Test_Game.java
+++ Test_Game.java
@@ -2,7 +2,7 @@ package Snake;
 
 import java.awt.*;
 import java.util.*;
-import org.junit.Ignore;
+
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -18,6 +18,7 @@ public class Test_Game {
     public void test_exist_Snake() {
         assertNotNull(game.Snake);
     }
+
     @Test
     public void test_Snake_size() {
         assertTrue(game.Snake.size() >= 3);
@@ -28,6 +29,7 @@ public class Test_Game {
     public void test_correct_Course(){
         assertNotNull(game.pCourse);
     }
+
     @Test
     public void test_Score(){
         assertEquals(game.Score, 0);
@@ -43,16 +45,11 @@ public class Test_Game {
          GameModel game = new GameModel(-5,-1001);
     }
 
-
-
-
-    /*
-    БЕЛЕБЕРДА КАКАЯ ТО С КООРДИНАТАМИ В ТЕСТЕ ПРО ГОЛОВУ
-    ПРАВИЛА УСТАНОВКИ ГОЛОВЫ: 1) Х и У СТОЯТ КАК ОБЫЧНО
-    2) НАЧАЛЬНАЯ КООРДИНАТА = game.width/2, game.height/2 + i
-     */
     @Test
-    public void test_correct_Snake_head_setting_1(){
+    public void test_initialization_3(){ GameModel game = new GameModel('k', 'a');  }
+
+    @Test
+    public void test_Snake_head_setting_1(){
         GameModel game_1 = new GameModel(1,7);
 
         assertTrue(game_1.Snake.get(0).x != 0 && game_1.Snake.get(0).y != 0);
@@ -61,47 +58,85 @@ public class Test_Game {
         assertTrue(game_1.Snake.get(0).x != game_1.width-1 && game_1.Snake.get(0).y != game_1.height - 1);
     }
 
-    @Ignore
     @Test
-    public void test_correst_Snake_head_setting_2(){
-        //проверка на то что голова не врезается в края сразу (3 клетки до любого края или ребра)
+    public void test_Snake_head_setting_2(){
         GameModel game = new GameModel(10,10);
+        int headX = game.Snake.get(0).x;
+        int headY = game.Snake.get(0).y;
 
+        if (game.width < 10 && game.width < 10)
+            return;
+        else{
+            if( 2 < headX && headX < game.width - 3 && 2 < headY && headY < game.height - 3 )
+                return;
+            else fail("Snake is too close to the boundaries");
+        }
     }
 
+    @Test
+    public void test_SpawnFood(){
+        game.existFood = false;
+
+        game.RefreshField();
+
+        assertTrue(game.existFood);
+    }
+
+    @Test
+    public void test_CheckOnEatSelf_method_1(){
+        game.RefreshField();
+
+        assertFalse(game.CheckOnEatSelf());
+    }
+
+    @Test
+    public void test_CheckOnEatSelf_method_2(){
+        if (game.SnakeLength >= 5) {
+            game.Set_Course("DOWN");
+
+            game.RefreshField();
+            game.Set_Course("LEFT");
+            game.RefreshField();
+            game.Set_Course("UP");
+            game.RefreshField();
+            game.Set_Course("RIGHT");
+            game.RefreshField();
+
+            assertTrue(game.CheckOnEatSelf());
+        }
+    }
 
     @Test
     public void test_correct_eating_1(){
-        GameModel game = new GameModel(10,10);
-        game.Food.x = 5;//это корректно, что х и у перепутаны местами или я запутался?
-        game.Food.y = 0;
-        int sLength = game.SnakeLength;
-        for (int i = 0; i < 5 ;i++){
-            game.Print();
-            game.RefreshField();
+        GameModel game = new GameModel(6,6);
+        game.Set_Course("UP");
 
-        }
+        game.Food.x = game.Snake.get(0).x;//это корректно, что х и у перепутаны местами или я запутался?
+        game.Food.y = game.Snake.get(0).y-1;
+        int sLength = game.SnakeLength;
+        int prevScore = game.Score;
         game.RefreshField();
-        assertFalse(game.Food == new Point(5,0));//проверка на съеденную еду и зареспавненную в др. точке
+
+        assertFalse(game.Food == new Point(game.Snake.get(0).x,game.Snake.get(0).y-1));
+                                // /проверка на съеденную еду и зареспавненную в др. точке
         assertEquals(game.SnakeLength,sLength+1);
+        assertNotEquals(game.Score, prevScore);
     }
 
     @Test
-    public void test_correct_reverse_direction(){
-        Map<String,String> reversedCourses = new HashMap<>();
-        reversedCourses.put("DOWN", "UP");
-        reversedCourses.put("UP", "DOWN");
-        reversedCourses.put("RIGHT", "LEFT");
-        reversedCourses.put("LEFT", "RIGHT");
+    public void test_reverse_direction(){
+        Map<Point,String> reversedCourses = new HashMap<>();
+        reversedCourses.put(new Point(0,1), "UP");
+        reversedCourses.put(new Point(0,-1), "DOWN");
+        reversedCourses.put(new Point(1,0), "LEFT");
+        reversedCourses.put(new Point(-1,0), "RIGHT");
         GameModel game = new GameModel(8, 8);
         Point original = game.pCourse;
 
         game.RefreshField();
-        game.RefreshField();
-        game.Set_Course(reversedCourses.get(game.pCourse));/* ты в словаре(строка, строка) обращаешься словать(поинт)*/
+        String reversedCourse = reversedCourses.get(game.pCourse);
+        game.Set_Course(reversedCourse);/* ты в словаре(строка, строка) обращаешься словать(поинт)*/
 
-        assertEquals(game.pCourse, original);
+        assertEquals("The course should not change on the opposite",game.pCourse, original);
     }
 }
-
-
