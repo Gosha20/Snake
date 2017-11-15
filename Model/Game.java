0ed8270ece314225d24dde3ba64b3864128d5ed1
Gosha20 <gosha3548@gmail.com>
2017-11-15 20:13:19 +0500

    добавил класс для игры с блоками, хочу потестить, боюсь сломать

---
+++ Model/Game.java
@@ -0,0 +1,105 @@
+package Snake.Model;
+
+import Snake.Constants;
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Game {
+    public Buff Buff;
+    public Snake Snake;
+    public boolean gameOver = false;
+    public int Score;
+    private final int height;
+    private final int width;
+    private int timeLiveBuff;
+    boolean existBuff;
+    private static Buff[] Buffs = new Buff[]{
+            new Buff("apple", 1, 20),
+            new Buff("poison", -1, 20),
+            new Buff("banan", 3, 15),
+            new Buff("grapes", 5,15),
+            new Buff("granat", 7,10)
+    };
+
+    public Game(int h, int w, int snakeLength){
+        this.Snake = new Snake(snakeLength);
+        this.existBuff = false;
+        this.height = h;
+        this.width = w;
+        SpawnFood();
+    }
+
+    public void RefreshField() {
+        timeLiveBuff--;
+        if (timeLiveBuff <= 0)
+            existBuff = false;
+
+        Snake.Move();
+        eatBuff();
+        fixPositionSnakeHead(Snake.GetHead());
+        SpawnFood();
+        if (checkOnLittleSnakeSize() || CheckOnEatSelf())
+            gameOver = true;
+    }
+
+
+    private void SpawnFood(){
+        Random rnd = new Random();
+        int n = rnd.nextInt(Buffs.length);
+        while (!existBuff){
+            int x = rnd.nextInt(height);
+            int y = rnd.nextInt(width);
+            Point tempBuff = new Point(x,y);
+            if (!Snake.body.contains(tempBuff))
+            {
+                Buff = Buffs[n];
+                Buff.x = tempBuff.x;
+                Buff.y = tempBuff.y;
+                existBuff = true;
+                timeLiveBuff = Buff.timeLive;
+            }
+        }
+    }
+
+    private void fixPositionSnakeHead(Point snakeHead){
+        if (snakeHead.x >= width)
+            snakeHead.x = 0;
+        if (snakeHead.x < 0)
+            snakeHead.x = width - 1;
+        if (snakeHead.y >= height)
+            snakeHead.y = 0;
+        if (snakeHead.y < 0)
+            snakeHead.y = height - 1;
+    }
+
+    private void eatBuff(){
+        if (Snake.GetHead().x == Buff.x && Snake.GetHead().y == Buff.y)
+        {
+            Score += Buff.countScore;
+            Snake.EatBuff(Buff);
+            existBuff = false;
+        }
+    }
+
+    private boolean CheckOnEatSelf(){
+        Point snakeHead = Snake.GetHead();
+        for (int i = 1; i<Snake.body.size();i++){
+            if (snakeHead.x == Snake.body.get(i).x && snakeHead.y == Snake.body.get(i).y)
+                return true;
+        }
+        return false;
+    }
+
+    private boolean checkOnLittleSnakeSize(){
+        return Snake.body.size() < Constants.minSnakeSize;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+}
---
+++ Model/GameWithBlocks.java
@@ -0,0 +1,37 @@
+package Snake.Model;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.Random;
+
+public class GameWithBlocks extends Game{
+    public ArrayList<Point> walls = new ArrayList<>();
+    public GameWithBlocks(int h, int w, int snakeLen){
+        super(h,w,snakeLen);
+        spawnWalls();
+    }
+    public void refreshField(){
+        super.RefreshField();
+        if (checkOnWall())
+            super.gameOver = true;
+    }
+
+    private boolean checkOnWall()
+    {
+        return walls.contains(Snake.GetHead());
+    }
+
+    private void spawnWalls(){
+        Random rnd = new Random();
+        for (int i = 0; i < super.getHeight(); i++){
+            Point tempWall;
+            do {
+                int x = rnd.nextInt(super.getHeight());
+                int y = rnd.nextInt(super.getWidth());
+                tempWall = new Point(x, y);
+            }
+            while (super.Snake.body.contains(tempWall));
+            walls.add(tempWall);
+        }
+    }
+}
